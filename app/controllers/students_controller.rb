c8b04ed12e441bab7b87bcfa17ce7db2071b8a00
Alec Magnet <amagnet@gmail.com>
2021-08-31 11:45:14 -0400

    commit tree 43c597d6fe762ac9dc220b690e1efc5a76d5acec

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,21 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    if params[:name].blank?
+      render json: students
+    else 
+      # byebug
+      found_student = Student.where("first_name LIKE ? or last_name LIKE ?", params[:name], params[:name])
+      render json: found_student
+    end
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  # def searched
+  #   student = Student.find_by(name: params[:name])
+  # end
 end
